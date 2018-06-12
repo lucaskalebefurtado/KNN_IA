353fe0b5d7c4e06d9844fbdb1f2387bfa3dcd45e
Lucas Kalebe <lucaskalebe13@gmail.com>
2018-06-12 11:38:00 -0300

    Agora permite que o usuário escolha o tamanho do k

--- knn.py
+++ knn.py
@@ -1,10 +1,12 @@
 #coding: utf-8
 # Lucas Kalebe Silva Furtado
-
-
+from collections import Counter
 import csv 
 from math import sqrt
 
+
+
+#abrir casos de treinamento
 def training():
     dadosTreinamento = []
     with open ('treinamento.csv') as csvfile:
@@ -13,7 +15,7 @@ def training():
             dadosTreinamento.append(row)
     return (dadosTreinamento)
 
-
+#abrir casos de teste
 def openTest():
     dadosTest = []
     with open ('teste.csv') as csvfile:
@@ -23,7 +25,7 @@ def openTest():
     return (dadosTest)
 
 
-
+#calcular a distancia entre dois pontos
 def calculaDistancia(lista1, lista2):
     distancia = 0
     for i in range(4):
@@ -31,30 +33,41 @@ def calculaDistancia(lista1, lista2):
         distancia2 = float(lista2[i])
         distancia += ((distancia1 - distancia2) ** 2)
     return sqrt(distancia)
-    
 
-def classificador(casoTeste):
-    treinamento = training()    
-    menorDistancia = 100000000000    
-    classificacao = "a"
+
+def classificador(casoTeste, k):
+    treinamento = training()    #abre o caso de treinamento
+    distancias = []             #armazena as distancias
+    for i in range(k):                      #preenche o array com valores muito grandes e uma classificação inexistente
+        distancias.append((10000000.0, "classifica"))
 
     for i in range(len(treinamento)):
-        if(i>0):
-            distancia = calculaDistancia(casoTeste, treinamento[i])
-            if(distancia < menorDistancia):
-                menorDistancia = distancia
-                classificacao = treinamento[i][4]
-    return (classificacao)
-        
+        if(i > 0): #linha da explicacao
+            distancia = calculaDistancia(casoTeste, treinamento[i]) #calcula a distancia entre os pontos
+            for j in range(k):
+               if ((distancias[j][0]) > (distancia)):
+                   distancias[j] = (distancia, treinamento[i][4])
+                   
+        print (distancias)           
+    
+   
+    classificacoes = [] # lista apenas com as classificacoes    
+    for i in range(k):
+        classificacoes.append(distancias[i][1]) #adiciona a lista apenas as classificacoes
+    cnt = Counter(classificacoes)
+    a = (cnt.most_common()) #conta o valor mais comum da lista
+    return (a[0][0]) # retorna a classificacao mais comum
+
 
 
 
 def KNN():
-    result = open('result.txt', 'w')
+    result = open('result.txt', 'w') #resultados
     teste = openTest()
+    k = int(input("Tamanho do K: ")) #tamanho do K a ser utilizado
     for i in range(len(teste)):
         if(i > 0):
-            result.writelines(classificador(teste[i])+"\n")
+            result.writelines(classificador(teste[i], k)+"\n")
     result.close()
         
 
@@ -73,7 +86,7 @@ def countMatches(results, bestResults):
         if(results[i] == bestResults[i]):
             count +=1
 
-    return (str(int(count/len(results) * 100)) + "% de acertos")
+    return (str(count/len(results) * 100) + "% de acertos")
 
 # Executar o KNN
 KNN()
