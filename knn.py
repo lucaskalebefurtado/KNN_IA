1000efb3cac9f6c0afa6553c2cdcafdce55b5cef
Lucas Kalebe <lucaskalebe13@gmail.com>
2018-06-13 01:28:24 -0300

    Correção no classificador

--- knn.py
+++ knn.py
@@ -4,9 +4,7 @@ from collections import Counter
 import csv 
 from math import sqrt
 
-
-
-#abrir casos de treinamento
+# abrir casos de treinamento
 def training():
     dadosTreinamento = []
     with open ('treinamento.csv') as csvfile:
@@ -28,7 +26,7 @@ def openTest():
 #calcular a distancia entre dois pontos
 def calculaDistancia(lista1, lista2):
     distancia = 0
-    for i in range(4):
+    for i in range(4):   
         distancia1 = float(lista1[i])
         distancia2 = float(lista2[i])
         distancia += ((distancia1 - distancia2) ** 2)
@@ -38,34 +36,26 @@ def calculaDistancia(lista1, lista2):
 def classificador(casoTeste, k):
     treinamento = training()    #abre o caso de treinamento
     distancias = []             #armazena as distancias
-    for i in range(k):                      #preenche o array com valores muito grandes e uma classificação inexistente
-        distancias.append((10000000.0, "classifica"))
-
-    for i in range(len(treinamento)):
+    for i in range(len(treinamento)):        
         if(i > 0): #linha da explicacao
             distancia = calculaDistancia(casoTeste, treinamento[i]) #calcula a distancia entre os pontos
-            for j in range(k):
-               if ((distancias[j][0]) > (distancia)):
-                   distancias[j] = (distancia, treinamento[i][4])
-                   
-        print (distancias)           
+            distancias.append((distancia, treinamento[i][4])) #adiciona na lista a tupla
+    distancias.sort(key=lambda tup: tup[0])  #organiza de acordo com as menores distancias
     
-   
-    classificacoes = [] # lista apenas com as classificacoes    
+    classificacoes = [] # lista apenas com as classificacoes
     for i in range(k):
-        classificacoes.append(distancias[i][1]) #adiciona a lista apenas as classificacoes
+        classificacoes.append(distancias[i][1]) #adiciona a lista apenas as k melhores classificacoes
+    
     cnt = Counter(classificacoes)
     a = (cnt.most_common()) #conta o valor mais comum da lista
-    return (a[0][0]) # retorna a classificacao mais comum
-
-
+    return (a[0][0]) # retorna a classificacao mais comum da lista
 
 
 def KNN():
-    result = open('result.txt', 'w') #resultados
+    result = open('result.txt', 'w') # escreve os resultados
     teste = openTest()
     k = int(input("Tamanho do K: ")) #tamanho do K a ser utilizado
-    for i in range(len(teste)):
+    for i in range(len(teste)): 
         if(i > 0):
             result.writelines(classificador(teste[i], k)+"\n")
     result.close()
@@ -85,7 +75,6 @@ def countMatches(results, bestResults):
     for i in range(len(results)):
         if(results[i] == bestResults[i]):
             count +=1
-
     return (str(count/len(results) * 100) + "% de acertos")
 
 # Executar o KNN
